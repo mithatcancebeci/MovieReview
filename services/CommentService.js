338a01c9f37f394922bba810dba828a534c0be17
mithatcancebeci <mithatcancebeci@gmail.com>
2021-02-28 21:45:25 +0300

    Update CommentService.js

--- services/CommentService.js
+++ services/CommentService.js
@@ -2,8 +2,11 @@ const CommentModel=require('../models/comments')
 
 exports.createComment=async(req,res,next)=>{
     const comment=await CommentModel.create(req.body);
-    
-    res.send(comment);
+    const commentUser = await  UserModel.findById({_id: comment.user})
+    commentUser.comments.push(comment);
+      await commentUser.save();
+    res.status(200).json({success:true, data: comment })
+
 }
 exports.getComments=async(req,res,next)=>{
     const comments=await CommentModel.find()
