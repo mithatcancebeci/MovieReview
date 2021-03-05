3175791292a2ef83f39a751365d6b4a70c8a81e9
mithatcancebeci <mithatcancebeci@gmail.com>
2021-03-05 18:19:57 +0300

    MovieDetails

---
+++ MRVue/movie/src/components/MovieDetail.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="movieDetail">
+    <div class="card" style="max-width: 1770px;">
+      <div class="row g-0">
+        <div class="col-md-3">
+          <img
+            :src="'https://image.tmdb.org/t/p/w300' + movieDetails.poster_path"
+            :alt="movieDetails.poster_path"
+          />
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title text-center">{{ movieDetails.title }}</h5>
+            <span>Genres :</span>
+            <div
+              class="genres"
+              :key="genres.id"
+              v-for="genres in movieDetails.genres"
+            >
+              <span>{{ genres.name }}</span>
+            </div>
+
+            <p class="card-text text-left">{{ movieDetails.overview }}</p>
+            <p v-for="trailer in movieVideos.results" :key="trailer.id">
+              <iframe
+                width="560"
+                height="315"
+                :src="'https://www.youtube.com/embed/'+trailer.key"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+              ></iframe>
+            </p>
+            <p class="card-text">
+              <small class="text-muted">{{ movieDetails.release_date }}</small>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ["movieDetails", "movieVideos"]
+};
+</script>
+<style scoped>
+.movieDetail {
+  margin-top: 100px;
+  margin-left: 10px;
+  margin-right: 10px;
+  background-color: #f42f42;
+}
+.col-md-3 {
+  margin-left: 50px;
+}
+
+.card-body {
+  text-align: center;
+}
+.card {
+  border: none;
+}
+.genres {
+  display: inline-flex;
+  margin-right: 5px;
+  transform: scale(0.7, 0.7);
+}
+.genres:hover {
+  background-color: #f42f42;
+  color: white;
+  transform: scale(1, 1);
+  border-radius: 180px;
+}
+.genres:hover > span {
+  border: none;
+  transform: scale(1, 1);
+}
+.genres > span {
+  border: 1px solid #f42f42;
+  padding: 15px;
+}
+@media screen {
+  .card {
+    max-width: 1770px;
+    min-width: 325px;
+  }
+}
+</style>
--- MRVue/movie/src/pages/Movie.vue
+++ MRVue/movie/src/pages/Movie.vue
@@ -1,27 +1,35 @@
 <script>
+import TopBar from '../components/TopBar'
+import MovieDetail from '../components/MovieDetail'
 import axios from 'axios'
 export default {
 name:"Movie",
   data(){
       return{
           movieDetails:{},
+          movieVideos:{}
          
       }
   },
-
- 
+  components:{TopBar,MovieDetail}
+ ,
   
   created(){
   
     axios.get(`https://api.themoviedb.org/3/movie/${this.$route.params.id}?api_key=63c20ab0688ef19ee1cdf0eacf135738&language=en-US`)
     .then((res)=>{console.log(res.data)
     this.movieDetails=res.data})
-  
+   
+  },
+  mounted(){
+    axios.get(`https://api.themoviedb.org/3/movie/${this.$route.params.id}/videos?api_key=63c20ab0688ef19ee1cdf0eacf135738&language=en-US`)
+   .then((res)=>{
+     this.movieVideos=res.data})
   }
 }
 </script><template>
     <div>
-       {{movieDetails.title}}
-      
-    </div>
+      <TopBar/>
+      <MovieDetail :movieDetails=movieDetails :movieVideos=movieVideos />
+</div>
 </template>
